5cfc8b37ac672b75c151caf23720fe912175500f
Emmanuel Ayodele <emmayodayo@gmail.com>
2018-09-05 15:02:24 +0100

    Initial commit, Added variables.js

---
+++ variables.js
@@ -0,0 +1,16 @@
+//A variable is a container for a value
+//A value can be an integer, a string etc
+//The values in a container can change with variables
+//Variables can be assigned with the let, const or var Keywords
+//let and const are newly introduced in ES6
+ 
+var myName;
+myName = "Emmanuel";
+
+var country = "Nigeria";
+
+console.log("My name is " + myName);
+
+//Variables can contain 0-9, a-z or even A-Z
+//Variables names should be written in camel case
+//

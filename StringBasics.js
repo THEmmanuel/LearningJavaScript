507ec0f3fc97acf27b656ee6b4491487c97ea2e9
Emmanuel Ayodele <emmayodayo@gmail.com>
2018-09-05 18:27:55 +0100

    Modified the StringBasics.js file

--- StringBasics.js
+++ StringBasics.js
@@ -9,7 +9,7 @@ var sentence = "\"Te Amo!\" She said to me"
 var sentence = '"Te Amo!" She said to me'
 //The two sentences above do the same thing.
 console.log(sentence);
-//This should output """Te Amo!" She said to me".
+//This should output " "Te Amo!" She said to me".
 
 /**
  * Others include
@@ -34,3 +34,19 @@ console.log(sentence);
 
  console.log(myFullName += " is my effin name");
  //This should output "Emmanuel Ayodele is my effin name"...... Sorry for swearing :)
+
+ //Strings [Bracket notation]
+ //Most programming languages don't start counting at 0NE , they start at ZERO
+ //This is refered to as Zero based index
+
+ console.log(myName[0]);
+ //This should output The first letter of myName which is "E"
+ //Note that the zero can be changed to 1 or 2 etc
+ //For example
+
+ console.log(myName[1]);
+ //This should output the second letter of myName which is "m"
+
+ //To get the last character, You need to subtract one from the total length of the string
+console.log(myName [myName.length - 1]);
+//This should output the last character of myName which is "l"

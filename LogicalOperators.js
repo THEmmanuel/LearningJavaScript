ebc00e9487fc78553cbfd2882bd0eb05c0932f61
Emmanuel Ayodele <emmayodayo@gmail.com>
2018-09-09 06:39:13 +0100

    Added LogicalOperators.js

---
+++ LogicalOperators.js
@@ -0,0 +1,12 @@
+// And is &&
+// Or is ||
+
+if (num > 7 && num < 15){
+    console.log("Logical operators are so coool");
+}
+//The if statement above checks if num is greater than seven AND less than fifteen
+
+/**
+ * Short circuit evaluations happen when conditions use AND && or OR ||
+ * 
+ */
